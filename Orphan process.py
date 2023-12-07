8529d90c8adba34371c7cd1528ca9c7f890e8df7
Kamali S <105334710+Kamali2003@users.noreply.github.com>
2023-12-07 20:36:22 +0530

    Create Orphan process.py

---
+++ Orphan process.py
@@ -0,0 +1,10 @@
+import os, sys, time
+ttlForParent = 50;
+for i in range(0, 10):
+  pid_1 = os.fork()
+  print("Hello Worlds!!!")
+  if pid_1 == 0:
+    sys.exit();
+  
+time.sleep(ttlForParent);
+os.wait()
